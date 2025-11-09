03e725bd66c53a76b49960fbdd7fd9d127ab0e3e
JanudaStack <jaariyasinghe2003@gmail.com>
2025-11-09 23:28:51 +0530

    feat: integrate Tailwind CSS and enhance Header component and add Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <>
-    </>
+    <header className="main-header">
+        <div className="logo">
+            <img src="/logo.png" alt="" />
+        </div>
+
+        <nav className="nav-menu">
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Portfolio</a>
+            <a href="#">Blog</a>
+        </nav>
+
+        <button className="cta-btn">
+            Get Free Consultation
+        </button>
+    </header>
   )
 }
 
