e3e9f30c3960b749a7dabcec63f738a93be350cc
JanudaStack <jaariyasinghe2003@gmail.com>
2025-11-10 21:59:36 +0530

    feat: enhance Header component layout and styles, and update font imports in CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,34 +2,29 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="main-header flex justify-between p-5">
-      <div className="logo">
-        <img src="/logo.png" alt="" className="w-10" />
-      </div>
-
-      <nav className="nav-menu hidden gap-8 justify-center  lg:flex">
-        <a href="#">About</a>
-        <a href="#">Services</a>
-        <a href="#">Portfolio</a>
-        <a href="#">Blog</a>
+    <header className="main-header flex items-center justify-between p-5">
+      <nav className="nav-menu">
+        <menu className="desktop-nav hidden lg:flex gap-5">
+          <a href="#">Projects</a>
+          <a href="#">Services</a>
+          <a href="#">Studio</a>
+          <a href="#">Insights</a>
+        </menu>
+        
+        <button className="menu lg:hidden">Menu</button>
+        <menu className="mobile-nav hidden ">
+          <a href="#">Projects</a>
+          <a href="#">Services</a>
+          <a href="#">Studio</a>
+          <a href="#">Insights</a>
+        </menu>
       </nav>
 
-      <button className="cta-btn bg-black rounded-4xl text-white px-3 hidden md:block">
-        Get Free Consultation
-      </button>
+      <div className="header-logo">
+        <img src="/logo.png" alt="zenith-logo" className="w-10" />
+      </div>
 
-      <button className="menu-btn md:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          width="30"
-          height="30"
-          viewBox="0 0 50 50"
-        >
-          <path d="M 0 7.5 L 0 12.5 L 50 12.5 L 50 7.5 Z M 0 22.5 L 0 27.5 L 50 27.5 L 50 22.5 Z M 0 37.5 L 0 42.5 L 50 42.5 L 50 37.5 Z"></path>
-        </svg>
-      </button>
+      <button className="contact-us lg:underline hover:no-underline">Contact</button>
     </header>
   );
 };
